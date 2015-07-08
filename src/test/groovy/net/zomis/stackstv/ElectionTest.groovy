e9361a38864c55934766455f89c760325bdc4fec
Simon Forsberg <zomis2k@hotmail.com>
2015-07-08 15:10:16 +0200

    cleaned up test

--- src/test/groovy/net/zomis/stackstv/ElectionTest.groovy
+++ src/test/groovy/net/zomis/stackstv/ElectionTest.groovy
@@ -4,34 +4,26 @@ import org.junit.Test
 
 class ElectionTest {
 
+    Election.ElectionResult runElection(String fileName) {
+        Election vote = Election.fromURL(getClass().classLoader.getResource(fileName))
+        vote.elect()
+    }
+
     @Test
     void test() {
         String fileName = 'stackoverflow-com-2015-election-results.blt'
-        Election vote = Election.fromURL(getClass().classLoader.getResource(fileName))
-        def result = vote.elect()
+        def result = runElection(fileName)
 
         def elected = result.getCandidates(Election.CandidateState.ELECTED)
         assert elected.size() == 3
         assert elected.stream().map({it.name}).toArray() == ['"meagar"', '"Martijn Pieters"', '"Jeremy Banks"']
-
-        result.rounds.each {
-            println it
-        }
+        ResultExport.export(new File(new File('output'), fileName + '.html'), fileName, result)
     }
 
     @Test
     void testExport() {
         String fileName = 'mini.blt'
-        Election vote = Election.fromURL(getClass().classLoader.getResource(fileName))
-        def result = vote.elect()
-
-        def elected = result.getCandidates(Election.CandidateState.ELECTED)
-        assert elected.size() == 3
-        assert elected.stream().map({it.name}).toArray() == ['"meagar"', '"Martijn Pieters"', '"Jeremy Banks"']
-
-        result.rounds.each {
-            println it
-        }
+        def result = runElection(fileName)
         ResultExport.export(new File(new File('output'), fileName + '.html'), fileName, result)
     }
 
