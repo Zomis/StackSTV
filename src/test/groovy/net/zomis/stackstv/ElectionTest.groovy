85ba04fd5d680eedbe66d9128416514d26cf9703
Simon Forsberg <zomis2k@hotmail.com>
2015-07-08 14:57:46 +0200

    added mini.blt and a specific test for outputting the HTML, to make it run faster

--- src/test/groovy/net/zomis/stackstv/ElectionTest.groovy
+++ src/test/groovy/net/zomis/stackstv/ElectionTest.groovy
@@ -17,7 +17,22 @@ class ElectionTest {
         result.rounds.each {
             println it
         }
-        ResultExport.export(new File(fileName + '.html'), fileName, result)
+    }
+
+    @Test
+    void testExport() {
+        String fileName = 'mini.blt'
+        Election vote = Election.fromURL(getClass().classLoader.getResource(fileName))
+        def result = vote.elect()
+
+        def elected = result.getCandidates(Election.CandidateState.ELECTED)
+        assert elected.size() == 3
+        assert elected.stream().map({it.name}).toArray() == ['"meagar"', '"Martijn Pieters"', '"Jeremy Banks"']
+
+        result.rounds.each {
+            println it
+        }
+        ResultExport.export(new File(new File('output'), fileName + '.html'), fileName, result)
     }
 
 }
