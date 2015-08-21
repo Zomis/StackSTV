6d34759a0db202cca40e6491be7652442afdd96a
Emmanuel Rosa <emmanuel_rosa@aol.com>
2015-08-21 05:13:32 -0400

    fix IteratorCategory comment

--- src/main/groovy/net/zomis/meta/IteratorCategory.groovy
+++ src/main/groovy/net/zomis/meta/IteratorCategory.groovy
@@ -1,23 +1,27 @@
 package net.zomis.meta
 
 /*
- * Returns a Closure which iterates while the condition Closure 
- * evaluates to true. The returned Closure expects another Closure, 
- * an action closure, as its single argument.
- * This 'action' Closure is called during each iteration and is passed 
- * the Iterator.next()value. 
- * When the iteration is complete, the Iterator is returned.
- *
- * Example usage: 
- * use(IteratorCategory) {
- *   def iter = reader.iterator().while { it != 'end' }.call { println it }
- * }
- *
- * @param condition Closure to evaluate on each iteration.
- * @return a Closure
+ * Adds useful methods to Iterator.
  */
 @groovy.lang.Category(Iterator)
 class IteratorCategory {
+
+    /*
+     * Returns a Closure which iterates while the condition Closure 
+     * evaluates to true. The returned Closure expects another Closure, 
+     * an action closure, as its single argument.
+     * This 'action' Closure is called during each iteration and is passed 
+     * the Iterator.next()value. 
+     * When the iteration is complete, the Iterator is returned.
+     *
+     * Example usage: 
+     * use(IteratorCategory) {
+     *   def iter = reader.iterator().while { it != 'end' }.call { println it }
+     * }
+     *
+     * @param condition Closure to evaluate on each iteration.
+     * @return a Closure
+     */
     Closure 'while'(Closure condition) {
         {Iterator iter, Closure closure ->
             while(iter.hasNext()) {
